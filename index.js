3036dc632b4256dd8d7d961c82347a603fc90712
claudiajerome07 <claudiajerome07@gmail.com>
2025-02-05 15:57:33 +0530

    assignment completed

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bcrypt = require('bcrypt')
+require('dotenv').config()
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+app.use(cors());
+app.use(express.json())
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log('DB connected'))
+  .catch((err) => console.Console.log(err))
+
+const userSchema = new mongoose.Schema({
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
+})
+const User = mongoose.model('User', userSchema)
+
+app.post('/login',async(req,res)=>{
+  try{
+    const {email,password}=req.body
+
+    if(!email || !password){
+      return res.status(400).send({messsage:'All the fields are required'})
+    }
+
+    const user=await User.findOne({email})
+    if(!user){
+      return res.status(404).send({messsage:'User not found',success:false})
+    }
+
+    const match=await bcrypt.compare(password,user.password)
+    if(!match){
+      return res.status(401).send({messsage:'Invalid user details'})
+    }
+    return res.status(200).send({messsage:'Login successfull',success:true})
+
+  }catch(err){
+    return res.status(500).send({messsage:'Internal server error',Error:err.messsage})
+  }
+})
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
